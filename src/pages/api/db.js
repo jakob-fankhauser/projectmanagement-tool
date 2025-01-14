782cb940a800bab8dfa014fe3d042cba5ab3f442
jakob-fankhauser <jakob.fankhauser@gmail.com>
2025-01-14 21:05:11 +0100

    Update db.js

--- src/pages/api/db.js
+++ src/pages/api/db.js
@@ -1,27 +1,5 @@
 import mysql from 'mysql2/promise';
 
-const parseConnectionString = (url) => {
-  const matches = url.match(/mysql:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/);
-  return {
-    user: matches[1],
-    password: matches[2],
-    host: matches[3],
-    port: parseInt(matches[4]),
-    database: matches[5]
-  };
-};
-
-const dbConfig = parseConnectionString(process.env.DATABASE_URL);
-
-const pool = mysql.createPool({
-  host: dbConfig.host,
-  user: dbConfig.user,
-  password: dbConfig.password,
-  database: dbConfig.database,
-  port: dbConfig.port,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-});
+const pool = mysql.createPool(process.env.DATABASE_URL);
 
 export default pool;
